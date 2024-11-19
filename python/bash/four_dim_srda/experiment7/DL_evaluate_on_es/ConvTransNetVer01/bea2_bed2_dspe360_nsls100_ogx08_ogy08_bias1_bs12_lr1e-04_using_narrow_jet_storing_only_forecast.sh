1c05bfec9b7648817d3d1624ad860b3f7e6d74d2
Arinori Notsu <notsu.a.aa@m.titech.ac.jp>
2024-11-19 17:39:07 +0900

    add shell script

---
+++ python/bash/four_dim_srda/experiment7/DL_evaluate_on_es/ConvTransNetVer01/bea2_bed2_dspe360_nsls100_ogx08_ogy08_bias1_bs12_lr1e-04_using_narrow_jet_storing_only_forecast.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Specify your root directory of this repository in the Singularity container.
+ROOT_DIR=$(pwd)
+
+IMAGE_PATH=${ROOT_DIR}/pytorch_es.sif
+
+EXPERIMENT_NAME=experiment7
+MODEL_NAME=ConvTransNetVer01
+
+BEA_NUM=2
+BED_NUM=2
+DESPE_NUM=360
+NSLS_NUM=100
+OGX_NUM=08
+OGY_NUM=08
+
+BIAS=1
+
+BS_NUM=12
+LR_NUM=1e-04
+
+SCRIPT_PATH=${ROOT_DIR}/python/scripts/four_dim_srda/srda/evaluate_using_fixed_obs_point_and_narrow_jet_storing_only_forecast.py
+
+CONFIG_DIR=${ROOT_DIR}/python/configs/four_dim_srda/${EXPERIMENT_NAME}
+
+CONFIG_SRDA_PATH=${CONFIG_DIR}/perform_4D_SRDA/${MODEL_NAME}/\
+bea${BEA_NUM}_bed${BED_NUM}_dspe${DESPE_NUM}_nsls${NSLS_NUM}_ogx${OGX_NUM}_ogy${OGY_NUM}_\
+bias${BIAS}_bs${BS_NUM}_lr${LR_NUM}.yml
+
+CONFIG_CFD_PATH=${CONFIG_DIR}/cfd_simulation/qg_model/gpu_evaluation_config.yml
+
+singularity exec \
+  --nv \
+  --env PYTHONPATH=${ROOT_DIR}/python \
+  --bind ${ROOT_DIR}:${ROOT_DIR} \
+  ${IMAGE_PATH} python3 ${SCRIPT_PATH} \
+  --config_srda_path ${CONFIG_SRDA_PATH} \
+  --config_cfd_path ${CONFIG_CFD_PATH} \
+  --model_name ${MODEL_NAME}
